ab83c4a0c9e9309e3c668f232acfe8df033f0851
enriquep95 <0169321@up.edu.mx>
2017-11-14 16:15:17 -0600

    hey :V

--- src/practicagit/PracticaGit.java
+++ src/practicagit/PracticaGit.java
@@ -5,19 +5,39 @@
  */
 package practicagit;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ol
  */
 public class PracticaGit {
 
+ 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+           
+        double area;
+        double r;
         
         System.out.println("Hola mundo");
+        
+        Scanner sc=new Scanner(System.in);
+                
+       
+                
+        System.out.println("Ingresa el radio");
+        r=sc.nextDouble();
+        area=(Math.PI*Math.pow(r, 2));
+        
+        System.out.println("El area del circulo es: "+area);
+ 
+        
+        
+      
        
     }
     
